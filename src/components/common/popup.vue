ea7e126216683a2f1c466c684302b6eaaf5e1349
Yuliya312 <yuliya_312@ukr.net>
2018-09-25 15:02:33 +0300

    T-07-popup

---
+++ src/components/common/popup.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="popup__template">
+    <div class="popup" v-show="active" >
+      <div class="popup__overlay" @click="close()">
+        <div class="popup__content">
+          <div class="popup__body" v-bind:class="getPopupClass(param)">
+            <slot></slot>
+          </div>
+          <div class="popup__footer">
+            <button class="btn" @click="onCancel()">{{ btnText }}</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <a href="#" class="link" @click="open()">some-href</a>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['btnText'],
+    data: function(){
+        return {
+            active: false
+        }
+    },
+    methods: {
+        open: function() {
+            this.active = true
+        },
+        close: function() {
+            this.active = false
+        },
+        onCancel: function() {
+            this.close()
+        },
+        getPopupClass: function(param) {
+            return param || 'info'
+        }
+    }
+}
+</script>
+
+<style>
+    .error {
+        color:red
+    }
+
+    .info {
+        color: rgb(27, 5, 150)
+    }
+
+    .success {
+        color: green;
+    }
+</style>
